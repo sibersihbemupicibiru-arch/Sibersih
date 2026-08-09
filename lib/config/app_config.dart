002e4f6d72100b09884597675d5a8c0d531078cc
mnaufalrizqullahabsil-pixel <mnaufalrizqullahabsil@gmail.com>
2026-08-09 18:59:56 +0700

    remove

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -10,9 +10,7 @@ abstract final class AppConfig {
       'https://ciaykezzojnksqlsioqh.supabase.co';
 
   static const String supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
-      '.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpYXlrZXp6b2pua3NxbHNpb3FoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3Njc1ODA2MiwiZXhwIjoyMDkyMzM0MDYyfQ'
-      '.glV4UI0HVHc4-id_uzFIeEqTQHuwTakOLbMNj2CnBfw';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpYXlrZXp6b2pua3NxbHNpb3FoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3NTgwNjIsImV4cCI6MjA5MjMzNDA2Mn0.vIeGdCi2d10Uds_wuJYfyLR2stjVjqZyFsyMRQ2DOCY';
 
   // ── Auth callback ─────────────────────────────────────────
   /// Deep-link scheme untuk native app (Android/iOS).
